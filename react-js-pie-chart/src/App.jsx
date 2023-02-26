51776bc772f7b5012bbc8a4a70d55a2df4a937e9
MamadTaheri68 <mamad.taheri.68@gmail.com>
2023-02-26 14:20:58 +0330

    add react project solution

---
+++ react-js-pie-chart/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Chart from "./components/Chart.component";
+
+const App = () => {
+   return (
+      <div className="container">
+         <h1>Pie Chart using Vanilla JavaScript</h1>
+         <Chart />
+      </div>
+   );
+};
+
+export default App;
---
+++ react-js-pie-chart/src/components/Chart.component.jsx
@@ -0,0 +1,28 @@
+import { useEffect } from "react";
+import { useState, useRef } from "react";
+
+const Chart = () => {
+   const chartRef = useRef();
+   const [percent, setPercent] = useState(30);
+   // background: conic-gradient(rgb(12, 40, 118) 30%, rgb(62, 106, 226) 0);
+
+   const changeHandler = (event) => {
+      setPercent(event.target.value);
+   };
+
+   useEffect(() => {
+      // console.log(chartRef.current)
+      chartRef.current.style.background = `conic-gradient(rgb(12, 40, 118) ${percent}%, rgb(62, 106, 226) 0)`
+   }, [percent])
+
+   return (
+      <>
+         <div className="chart" ref={chartRef}>
+            <div className="legends">Value: {percent}%</div>
+         </div>
+         <input type="range" className="range-selector" value={percent} onChange={changeHandler} />
+      </>
+   );
+};
+
+export default Chart;
